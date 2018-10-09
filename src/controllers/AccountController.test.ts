2bd3f18db26495b95c64b847b832aa62b1e6b30e
yaronsha <yaron.shapira7@gmail.com>
2018-10-09 16:57:57 +0300

    feat: add tests to account controller. DD-284

--- src/controllers/AccountController.test.ts
+++ src/controllers/AccountController.test.ts
@@ -38,5 +38,65 @@ describe('AccountController class', () => {
       expect(authMock.generateSignedToken).toHaveBeenCalledWith(driver);
       expect(responseMockInstance.send).toBeCalledWith(200, {token});
     });
+
+    it('should send userAuthenticated false when get invalid password', async () => {
+      const email = 'test@dav.network';
+      const password = 'notfunindav1234';
+      const hashedPassword = '$2b$10$K6BIhI1E6tBtsiWvBlh/zuN.C.2yT5Cu/GCOJHs9419Bm1PefvPyu';
+      const cassandra = require('../cassandra/drivers');
+      cassandra.findByEmail = jest.fn((emailParam: string) => driver);
+      const authMock = {
+        generateSignedToken: jest.fn((driverParam: IDriver) => token),
+      };
+      jest.doMock('../lib/auth', () => authMock);
+      const accountController = (await import('./AccountController')).default;
+      const token = 'token';
+      const driver: IDriver = {id: 'id', password: hashedPassword};
+      const requestMock = jest.fn<Request>(() => ({
+        body: { email, password },
+      }));
+      const requestMockInstance = new requestMock();
+      const responseMock = jest.fn<Response>(() => ({
+        send: jest.fn(() => ''),
+      }));
+      const responseMockInstance = new responseMock();
+      const accountControllerInstance = new accountController();
+
+      await accountControllerInstance.authenticateDriver(requestMockInstance, responseMockInstance);
+
+      expect(cassandra.findByEmail).toHaveBeenCalledWith(email);
+      expect(authMock.generateSignedToken).toHaveBeenCalledTimes(0);
+      expect(responseMockInstance.send).toBeCalledWith(200, {userAuthenticated: false});
+    });
+
+    it('should send userAuthenticated false when get invalid email', async () => {
+      const email = 'invalid@dav.network';
+      const password = 'funindav1234';
+      const hashedPassword = '$2b$10$K6BIhI1E6tBtsiWvBlh/zuN.C.2yT5Cu/GCOJHs9419Bm1PefvPyu';
+      const cassandra = require('../cassandra/drivers');
+      cassandra.findByEmail = jest.fn((emailParam: string) => null);
+      const authMock = {
+        generateSignedToken: jest.fn((driverParam: IDriver) => token),
+      };
+      jest.doMock('../lib/auth', () => authMock);
+      const accountController = (await import('./AccountController')).default;
+      const token = 'token';
+      const driver: IDriver = {id: 'id', password: hashedPassword};
+      const requestMock = jest.fn<Request>(() => ({
+        body: { email, password },
+      }));
+      const requestMockInstance = new requestMock();
+      const responseMock = jest.fn<Response>(() => ({
+        send: jest.fn(() => ''),
+      }));
+      const responseMockInstance = new responseMock();
+      const accountControllerInstance = new accountController();
+
+      await accountControllerInstance.authenticateDriver(requestMockInstance, responseMockInstance);
+
+      expect(cassandra.findByEmail).toHaveBeenCalledWith(email);
+      expect(authMock.generateSignedToken).toHaveBeenCalledTimes(0);
+      expect(responseMockInstance.send).toBeCalledWith(200, {userAuthenticated: false});
+    });
   });
 });
